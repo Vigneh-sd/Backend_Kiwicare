e190e56375b57b0470dd6a460673d8feef23a4e8
Vigneh-sd <svigneh8@gmail.com>
2025-06-16 13:37:12 +0530

    fix: updated packages for Vercel deploy

--- src/main/java/com/kiwicare/localhelp/config/CorsConfig.java
+++ src/main/java/com/kiwicare/localhelp/config/CorsConfig.java
@@ -14,8 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
-                        .allowedMethods("*")
+                        .allowedOrigins(
+                            "http://localhost:5173",
+                            "https://kiwicare-frontend.vercel.app"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
